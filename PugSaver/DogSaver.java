b86ac79dc2ef40fdab762053aafbe7bfd0d94eb3
Andrew Theiss <andrew.theiss@gmail.com>
2025-08-28 08:41:03 -0700

    Addition Aug 28

--- PugSaver/DogSaver.java
+++ PugSaver/DogSaver.java
@@ -4,8 +4,11 @@ public class DogSaver {
     public static void main(String[] args) {
         ArrayList notDogs = new ArrayList();
         
+        for (int i = 0; i <asdlkfjasld numDogs.length; i++) {
+            notDogs.add(dogslist[i])
+        }
 
-
+        notDogs.
         
     }
 }
